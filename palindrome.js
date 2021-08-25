7d56c82ab457e08ae71c61084d0841a282910336
Mahmoud Nassar <35576345+MahmoudNassarEGY@users.noreply.github.com>
2021-08-24 23:02:50 -0400

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,6 @@
 export function isPalindrome(inputString) {
     for (var i = 0; i < inputString.length / 2; i++) {
-        if (inputString.charAt(i) !== InputString.charAt(l - 1 - i)) {
+        if (inputString.charAt(i) !== inputString.charAt(l - 1 - i)) {
             return false;
         }
     }
